f72b5714c4c2be26d193530248d0158512fee71d
Mike Hudgins <mchudgins@gmail.com>
2017-11-02 18:09:34 -0500

    Initial commit of etcd bakery

---
+++ etcd/ubuntu-cloud-init.sh
@@ -0,0 +1,138 @@
+#! /bin/bash
+
+VAULT_ARTIFACT="s3://dstcorp/artifacts/vault-0.8.3.zip"
+ETCD_ARTIFACT="s3://dstcorp/artifacts/etcd-3.2.7-custom"
+
+# hmmmm, need to set the hostname to something the AWS DNS server knows
+sudo hostname `hostname -s`.ec2.internal
+
+echo 'Initial Disk Summary'
+df -H
+
+echo 'Starting Package Installations'
+
+#   update package info
+sudo apt-get update -yq
+
+# the grub package doesn't respect -y by itself, so we need a bunch of extra options,
+# or the provisioner will get stuck at an interactive prompt asking about Grub configuration
+# see http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -yq
+sudo apt-get install -yq --no-install-recommends \
+  apt-transport-https awscli \
+  bash-completion ca-certificates curl e2fsprogs ethtool htop jq \
+  linux-image-extra-virtual nano \
+  net-tools tcpdump unzip
+
+# stop unattended upgrades -- that's why we have baked images!
+sudo apt-get remove -yq unattended-upgrades
+sudo apt-get autoremove -yq
+
+# DST Root CA
+aws s3 cp s3://dstcorp/dst-root.crt /tmp
+sudo cp /tmp/dst-root.crt /usr/local/share/ca-certificates
+sudo update-ca-certificates
+
+# change the journald options to have only one log file
+# rather than one per user
+sudo sh -c 'echo "SplitMode=none" >>/etc/systemd/journald.conf'
+
+# create a non-privileged etcd user
+sudo adduser --system --home /var/lib/etcd --gecos 'etcd,,,' --disabled-password etcd
+
+# install & configure VAULT as a single (non-HA) instance using s3 as the backing storage
+aws s3 cp ${VAULT_ARTIFACT} /tmp/vault.zip \
+    && unzip /tmp/vault.zip \
+    && sudo mv vault /usr/local/bin \
+    && aws s3 cp ${ETCD_ARTIFACT} /tmp/etcd \
+    && chmod +x /tmp/etcd \
+    && sudo mv /tmp/etcd /usr/local/bin \
+    && sudo mkdir -p /usr/local/etc/etcd \
+    && sudo mkdir -p /var/lib/etcd \
+    && sudo chown etcd /var/lib/etcd \
+    && sudo chown root.root /usr/local/bin/*
+
+# set up the systemd service file for the etcd service
+cat <<"EOF" >/tmp/etcd.service
+[Unit]
+Description=etcd distributed consensus service
+
+[Service]
+EnvironmentFile=-/etc/default/etcd
+User=etcd
+Group=nogroup
+ExecStart=/usr/local/bin/etcd $OPTS
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=process
+Restart=on-failure
+RestartPreventExitStatus=255
+Type=simple
+EOF
+sudo mv /tmp/etcd.service /etc/systemd/system
+sudo systemctl daemon-reload
+
+# set up the environment file for etcd
+cat <<"EOF" >/tmp/etcd.env
+CA=/usr/local/share/ca-certificates/root-ca.crt
+CERT=/usr/local/etc/etcd/cert.pem
+KEY=/usr/local/etc/etcd/key.pem
+INTERNAL_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+ETCD_NAME=etcd
+
+OPTS="--name ${ETCD_NAME} \
+  --cert-file=${CERT} \
+  --key-file=${KEY} \
+  --peer-cert-file=${CERT} \
+  --peer-key-file=${KEY} \
+  --trusted-ca-file=${CA} \
+  --peer-trusted-ca-file=${CA} \
+  --peer-client-cert-auth \
+  --client-cert-auth \
+  --initial-advertise-peer-urls https://${INTERNAL_IP}:2380 \
+  --listen-peer-urls https://${INTERNAL_IP}:2380 \
+  --listen-client-urls https://${INTERNAL_IP}:2379,https://127.0.0.1:2379 \
+  --advertise-client-urls https://${INTERNAL_IP}:2379 \
+  --initial-cluster-token etcd-cluster-0 \
+  --initial-cluster controller-0=https://10.10.128.10:2380,controller-1=https://10.10.128.11:2380,controller-2=https://10.10.128.12:2380 \
+  --initial-cluster-state new \
+  --data-dir=/var/lib/etcd"
+EOF
+sudo cp /tmp/etcd.env /etc/default/etcd
+
+# use rc.local to launch vault
+sudo systemctl enable rc-local.service
+
+# create the rc.local start script
+cat <<"EOF" >/tmp/rc.local
+#! /bin/bash
+hostname `hostname -s`.ec2.internal
+
+if [[ ! -f /etc/default/etcd ]]; then
+    REGION=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[abcdefghijk]$//'`
+
+    # create the initial config for the seed instance of vault
+    # (normally we should use spring cloud config server for this,
+    # but it won't be running yet!)
+cat <<EOF_CFG >/etc/default/etcd
+EOF_CFG
+
+    aws --region ${REGION} s3 cp s3://io.dstcorp.vault.${REGION}/cert.pem /usr/local/etc/etcd/cert.pem
+    aws --region ${REGION} s3 cp s3://io.dstcorp.vault.${REGION}/key.pem /usr/local/etc/etcd/key.pem
+    chmod og-rw /usr/local/etc/etcd/key.pem
+    chmod u-w /usr/local/etc/etcd/key.pem
+    chown -R etcd /usr/local/etc/etcd
+fi
+
+#sudo -u vault /usr/local/bin/vault server -config=${VAULT_CONFIG} &
+systemctl start etcd
+EOF
+sudo cp /tmp/rc.local /etc/rc.local
+sudo chmod +x /etc/rc.local
+
+# clean up
+sudo apt-get clean
+sudo rm -r /var/lib/apt/lists/*
+rm -rf /tmp/*
+
+echo 'Disk Summary after Update'
+df -H
---
+++ etcd/va-spot.sh
@@ -0,0 +1,152 @@
+#! /bin/bash
+
+# check if a spot price was provided
+if [[ -z "${SPOT_PRICE}" ]]; then
+    BID_PRICE=0.0275
+else
+    BID_PRICE=${SPOT_PRICE}
+fi
+
+#
+# configuration
+#
+
+REGION=us-east-1
+INSTANCE_TYPE=m4.large
+KEY_NAME="kp201707"
+SUBNET="subnet-08849b7f"
+
+if [[ ! /bin/true ]]; then
+    echo SUBNET=${SUBNET}
+    exit 0
+fi
+
+# retrieve the list of ami's owned by this account
+IMAGES=`aws --region ${REGION} ec2 describe-images --owners self`
+#IMAGES=`cat /tmp/images.json`
+
+# now find the latest image matching vault-seed-<date/time>
+ORIGIN_DATE="0000-00-00T00:00:00.000Z"
+MAX_DATE=${ORIGIN_DATE}
+MAX_IMAGE_INDEX=-1
+
+image_count=`echo ${IMAGES} | jq '.[] | length'`
+for i in `seq 1 ${image_count}`; do
+  var=`expr $i - 1`
+  name=`echo ${IMAGES} | jq .Images[$var].Name`
+  IFS='-' read -ra NAME <<< "${name//\"/}"
+  if [[ ${#NAME[@]} -eq 2 ]]; then
+    if [[ ${NAME[0]} == "etcd" ]]; then
+      IMAGE_DATE=`echo ${IMAGES} | jq .Images[$var].CreationDate`
+      if [[ "${MAX_DATE}" < "${IMAGE_DATE}" ]]; then
+        MAX_DATE=${IMAGE_DATE}
+        MAX_IMAGE_INDEX=${var}
+      fi
+    fi
+  fi
+done
+IMAGE_NAME=`echo ${IMAGES} | jq .Images[${MAX_IMAGE_INDEX}].Name`
+IMAGE_ID=`echo ${IMAGES} | jq .Images[${MAX_IMAGE_INDEX}].ImageId`
+echo "Launching ${IMAGE_NAME} (${IMAGE_ID})"
+
+# create the cloud-init user data
+USERDATA=$(cat <<-"_EOF_" | sed -e "s/REGION=xxx/REGION=${REGION}/" | base64 -w 0
+#! /bin/bash
+REGION=xxx
+id >/tmp/id.uid
+date > /tmp/cloud-final
+#hostname vault-seed-${REGION}.dstcorp.io
+_EOF_
+)
+
+echo ${USERDATA} | base64 -d
+
+# create the updated json launch config in a temp file
+FILE=`mktemp`
+cat <<EOF >${FILE}
+{
+    "ImageId": ${IMAGE_ID},
+    "KeyName": "${KEY_NAME}",
+    "UserData": "${USERDATA}",
+    "InstanceType": "${INSTANCE_TYPE}",
+    "IamInstanceProfile": {
+        "Name": "ec2PackerInstanceRole"
+    },
+    "EbsOptimized": true,
+    "Monitoring": {
+        "Enabled": false
+    },
+    "NetworkInterfaces": [
+      {
+        "DeviceIndex": 0,
+        "SubnetId": "${SUBNET}",
+        "PrivateIpAddress": "10.10.128.10",
+        "Groups": [
+            "sg-5ef8153a"
+            ]
+      }
+    ]
+}
+EOF
+
+cat ${FILE}
+
+cmd="aws --region ${REGION} ec2 request-spot-instances --spot-price ${BID_PRICE} --instance-count 1 --type one-time --launch-specification file://${FILE}"
+echo cmd = ${cmd}
+rc=`${cmd}`
+echo $rc >/tmp/ohio.json
+
+requestID=`echo ${rc} | jq .SpotInstanceRequests[0].SpotInstanceRequestId | sed -e 's/"//g'`
+if [[ -z "${requestID}" ]]; then
+    echo "Unable to find Spot Request ID"
+    exit 1
+fi
+
+rm ${FILE}
+
+# wait up to 5 minutes for an instance & either tag it or exit with error
+sleep 15
+instanceID=`aws --region ${REGION} ec2 describe-spot-instance-requests --spot-instance-request ${requestID} \
+    | jq .SpotInstanceRequests[0].InstanceId | sed -e 's/"//g'`
+echo instanceID ${instanceID}
+
+while [[ "${instanceID}" == "null" ]]
+  do
+  sleep 5
+  instanceID=`aws --region ${REGION} ec2 describe-spot-instance-requests --spot-instance-request ${requestID} \
+    | jq .SpotInstanceRequests[0].InstanceId | sed -e 's/"//g'`
+  done
+aws --region ${REGION} ec2 create-tags --resources ${instanceID} --tags Key=Name,Value=vault-seed
+
+#display the instance's IP ADDR
+ipaddr=`aws --region ${REGION} ec2 describe-instances --instance-ids ${instanceID} | jq .Reservations[0].Instances[0].PublicIpAddress`
+echo Instance availabe at ${ipaddr}
+
+# update the DNS entry for this new instance of vault-seed-${REGION}.dstcorp.io
+FILE=`mktemp`
+cat <<EOF >${FILE}
+{
+    "Comment": "Update record to reflect public IP address of instance ${instanceID}",
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "vault-seed-${REGION}.dstcorp.io.",
+                "Type": "A",
+                "TTL": 300,
+                "ResourceRecords": [
+                    {
+                        "Value": ${ipaddr}
+                    }
+                ]
+            }
+        }
+    ]
+}
+EOF
+
+ZONEID=`aws route53 list-hosted-zones | jq '.[][] | select(.Name=="dstcorp.io.") | .Id' | sed  -s 's/"//g' | sed -s 's|/hostedzone/||g'`
+#aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch file://${FILE}
+#aws --region ${REGION} ec2 create-tags --resources ${instanceID} --tags Key=DNS,Value=vault-seed-${REGION}.dstcorp.io
+
+rm ${FILE}
