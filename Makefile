76550588cb626f414301bf3a155117530dfd3923
Mike Hudgins <mchudgins@gmail.com>
2017-11-26 10:49:55 -0600

    Bake a plain ubuntu 17.10

--- Makefile
+++ Makefile
@@ -10,11 +10,12 @@ all: $(TARGETS)
 	@echo $(TARGETS)
 
 base.ami: aws-bake.json ubuntu/cloud-init.sh
-	@echo Need to make a new $@ image
+	bin/bake --parent ami-534bc129 --image-stream ubuntu --artifact-version 17.10 \
+		--description 'base image from Ubuntu 17.10' ubuntu/cloud-init.sh
 
 dev.ami: base.ami aws-bake.json dev/cloud-init.sh
 	@echo Need to make a new dev image
 	bin/bake --parent $(shell cat base.ami) --image-stream dev --description 'Dev image base on Ubuntu' dev/cloud-init.sh
 
-origin-master.ami: ubuntu.ami dev/ubuntu-dev.json dev/ubuntu-dev-cloud-init.sh
+origin-master.ami: ubuntu.ami aws-bake.json origin-master/ubuntu-dev-cloud-init.sh
 	@echo Need to make a new dev image
---
+++ ubuntu/cloud-init.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+# hmmmm, need to set the hostname to something the AWS DNS server knows
+sudo hostname `hostname -s`.ec2.internal
+
+echo 'Kernel Info: ' `uname -a`
+
+echo 'Initial Disk Summary'
+df -H
+
+echo 'Starting Package Installations'
+
+#   set up bazel package repo
+#echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+#curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+
+#   update package info
+sudo apt-get update -yq
+
+# the grub package doesn't respect -y by itself, so we need a bunch of extra options,
+# or the provisioner will get stuck at an interactive prompt asking about Grub configuration
+# see http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -yq
+sudo apt-get install -yq --no-install-recommends \
+  apt-transport-https awscli \
+  bash-completion ca-certificates curl e2fsprogs ethtool htop jq \
+  linux-image-extra-virtual nano \
+  net-tools tcpdump unzip
+
+# stop unattended upgrades -- that's why we have baked images!
+sudo apt-get remove -yq unattended-upgrades
+sudo apt autoremove -yq
+
+# DST Root CA
+aws s3 cp s3://dstcorp/dst-root.crt /tmp
+sudo cp /tmp/dst-root.crt /usr/local/share/ca-certificates
+sudo update-ca-certificates
+
+# clean up
+sudo apt-get autoremove
+sudo apt-get clean
+sudo rm -r /var/lib/apt/lists/*
+rm -rf /tmp/*
+
+echo 'Disk Summary after Update'
+df -H
