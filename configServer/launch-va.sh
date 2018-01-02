335e5220077de7d5ab7e88619f6f71727fa6362d
Mike Hudgins <mchudgins@gmail.com>
2018-01-02 10:27:30 -0600

    Update config server launch script for a mgmt subnet

--- configServer/launch-va.sh
+++ configServer/launch-va.sh
@@ -6,13 +6,9 @@
 
 REGION=us-east-1
 INSTANCE_TYPE=t2.nano
-SPOT_PRICE=0
 KEY_NAME="kp201707"
-SUBNET="subnet-08849b7f"
 IMAGE_STREAM=dev
-CLUSTER_NAME=vpc0
-NODE_IP="10.10.128.6"
-IAM_INSTANCE_PROFILE=ec2PackerInstanceRole
+CLUSTER_NAME=o7t-alpha
 
 #
 # flags from command line may supersede defaults
@@ -27,6 +23,11 @@ while test $# -gt 0; do
 
         -h|--help)
             echo `basename $0` '--region (us-east-1|us-west-2)'
+            ;;
+
+        --image-stream)
+            shift
+            IMAGE_STREAM=$1
             ;;
 
         --instance-type)
@@ -49,26 +50,6 @@ while test $# -gt 0; do
             SPOT_PRICE=$1
             ;;
 
-        --subnet)
-            shift
-            SUBNET=$1
-            ;;
-
-        --image-stream)
-            shift
-            IMAGE_STREAM=$1
-            ;;
-
-        --iam-profile)
-            shift
-            IAM_INSTANCE_PROFILE=$1
-            ;;
-
-        --node-ip)
-            shift
-            NODE_IP=$1
-            ;;
-
          *)
             break
             ;;
@@ -83,6 +64,36 @@ source ../helpers/bash_functions
 IMAGE_ID=$(mostRecentAMI ${IMAGE_STREAM})
 echo "Launching ${IMAGE_ID}"
 
+# compute the subnet id from the subnet name
+AWSDATA=`aws ec2 describe-subnets --filters Name=tag:Name,Values=${CLUSTER_NAME}-mgmt`
+SUBNET=`echo ${AWSDATA} | jq .Subnets[0].SubnetId | sed -s 's/"//g'`
+VPC=`echo ${AWSDATA} | jq .Subnets[0].VpcId | sed -s 's/"//g'`
+CIDR=`echo ${AWSDATA} | jq .Subnets[0].CidrBlock | sed -e 's^.0/24^^' -e 's/"//g'`
+NODE_IP="${CIDR}.10"
+echo SUBNET=${SUBNET} VPC=${VPC}
+if [[ -z "${SUBNET}" || "${SUBNET}" == "null" ]]; then
+    echo "Unable to find the private subnet (${CLUSTER_NAME}-private) within the ${CLUSTER_NAME} cluster"
+    exit 1
+fi
+
+# find the security group(s)
+    # first we need to find the name of the vpc
+vpcinfo=$(aws --region ${REGION} ec2 describe-vpcs --filters Name=vpc-id,Values=${VPC})
+vpcName=`echo ${vpcinfo} | jq '.Vpcs[0].Tags | from_entries.Name' | sed -e 's/"//g'`
+
+if [[ -z "${vpcName}" ]]; then
+    echo "Unable to find the vpc Name tag within vpc ${VPC}"
+    exit 2
+fi
+
+    # then we can find the security group as <vpc-name>-vault
+
+CONFIG_SECURITY_GROUP=`aws --region ${REGION} ec2 describe-security-groups \
+    --filters Name=group-name,Values=${vpcName}-configServer | jq .SecurityGroups[0].GroupId | sed -e 's/"//g'`
+NODE_EXPORTER_SECURITY_GROUP=`aws --region ${REGION} ec2 describe-security-groups \
+    --filters Name=group-name,Values=${vpcName}-prometheus-monitored-instance | jq .SecurityGroups[0].GroupId | sed -e 's/"//g'`
+
+
 # create the cloud-init user data
 #USERDATA=$(cat <<-_EOF_ | base64 -w 0
 ##! /usr/bin/env bash
@@ -96,20 +107,17 @@ echo "Launching ${IMAGE_ID}"
 #    mchudgins/configserver:0.0.2-SNAPSHOT
 #_EOF_
 #)
-$(cat <<-_EOF_ >/tmp/userdata
+USERDATA=$(cat <<-"_EOF_" | sed -e "s/REGION=xxx/REGION=${REGION}/" | base64 -w 0
 #cloud-config
 runcmd:
- - 'aws s3 cp s3://dstcorp/artifacts/node_exporter-0.15.1.linux-amd64.tar.gz ./node-ex.tar.gz
- && tar xfz node-ex.tar.gz
- && ./node_exporter-0.15.1.linux-amd64/node_exporter &'
  - docker run -d -e GIT_REPO_URL=https://github.com/mchudgins/config-props.git -p 80:8888 mchudgins/configserver:0.0.2-SNAPSHOT
 _EOF_
 )
 
-#echo ${USERDATA} | base64 -d
+echo ${USERDATA} | base64 -d
 
 # create the updated json launch config in a temp file
-#    "EbsOptimized": true,
+EBS_OPTIMIZED=`isEBSOptimizable ${INSTANCE_TYPE}`
 FILE=`mktemp`
 cat <<EOF >${FILE}
 {
@@ -119,18 +127,20 @@ cat <<EOF >${FILE}
     "InstanceType": "${INSTANCE_TYPE}",
     "InstanceInitiatedShutdownBehavior": "terminate",
     "IamInstanceProfile": {
-        "Name": "${IAM_INSTANCE_PROFILE}"
+        "Name": "ec2PackerInstanceRole"
     },
+    "EbsOptimized": ${EBS_OPTIMIZED},
     "Monitoring": {
         "Enabled": false
     },
     "NetworkInterfaces": [
       {
+        "AssociatePublicIpAddress": true,
         "DeviceIndex": 0,
         "SubnetId": "${SUBNET}",
         "PrivateIpAddress": "${NODE_IP}",
         "Groups": [
-            "sg-5ef8153a"
+            "${CONFIG_SECURITY_GROUP}", "${NODE_EXPORTER_SECURITY_GROUP}"
             ]
       }
     ]
@@ -139,17 +149,29 @@ EOF
 
 cat ${FILE}
 
-if [[ "${SPOT_PRICE}" == "0" ]]; then
-# launch an on demand instance
-    instanceID=$(launchInstance ${REGION} ${FILE} /tmp/userdata)
-else
-# launch the spot instance
-    instanceID=$(launchSpotInstance ${REGION} ${SPOT_PRICE} ${FILE})
-fi
+# launch the instance. if a t2.* type use an on-demand instance, otherwise make a spot request
+IFS="." read -r -a el <<< "${INSTANCE_TYPE}"
+case "${el[0]}" in
+    t2)
+        echo "Launching an on-demand instance"
+        USERDATAFILE=`mktemp`
+        echo ${USERDATA} | base64 -d >${USERDATAFILE}
+        instanceID=$(launchInstance ${REGION} ${FILE} ${USERDATAFILE})
+        if [[ $? -ne 0 ]]; then
+            echo "Error occurred; exiting."
+            exit 1
+        fi
+        rm ${USERDATAFILE}
+        ;;
 
-if [[ $? != 0 ]]; then
-    exit 1
-fi
+    *)
+        echo "Launching a spot instance"
+        instanceID=$(launchSpotInstance ${REGION} ${BID_PRICE} ${FILE})
+        if [[ $? != 0 ]]; then
+            exit 1
+        fi
+        ;;
+esac
 
 echo instanceID ${instanceID}
 
@@ -159,8 +181,8 @@ rm ${FILE}
 aws --region ${REGION} ec2 create-tags --resources ${instanceID} --tags Key=Name,Value="configServer"
 
 #display the instance's IP ADDR
-ipaddr=`aws --region ${REGION} ec2 describe-instances --instance-ids ${instanceID} | jq .Reservations[0].Instances[0].PublicIpAddress`
-echo Instance available at ${ipaddr}
+#ipaddr=`aws --region ${REGION} ec2 describe-instances --instance-ids ${instanceID} | jq .Reservations[0].Instances[0].PublicIpAddress`
+#echo Instance available at ${ipaddr}
 
 # update the DNS entry for this new instance of vault-seed-${REGION}.dstcorp.io
-upsertDNS "config-${REGION}.dstcorp.io." ${ipaddr} ${instanceID}
+#upsertDNS "config-${REGION}.dstcorp.io." ${ipaddr} ${instanceID}
